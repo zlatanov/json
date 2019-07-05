f28dda70d52ed6abca244a4e7e5be37c48306b8c
Ivan Zlatanov <ivan@zlatanov.net>
2019-07-05 18:36:26 +0300

    Unsealing JsonIgnoreAttribute allowing custom attributes to extend it.

--- src/JsonIgnoreAttribute.cs
+++ src/JsonIgnoreAttribute.cs
@@ -6,7 +6,7 @@ namespace Maverick.Json
     /// Instructs the <see cref="JsonSettings"/> not to serialize the public field or public read/write property value.
     /// </summary>
     [AttributeUsage( AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false )]
-    public sealed class JsonIgnoreAttribute : Attribute
+    public class JsonIgnoreAttribute : Attribute
     {
     }
 }
