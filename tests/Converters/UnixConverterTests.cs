801cad2d11319f5fa290407159fc69f6da455625
Ivan Zlatanov <ivan@zlatanov.net>
2020-01-13 17:59:38 +0200

    Add converter for Unix times.

---
+++ tests/Converters/UnixConverterTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+
+namespace Maverick.Json.Converters
+{
+    public class UnixConverterTests
+    {
+        [Fact]
+        public void Convert()
+        {
+            var time = new DateTime( 2000, 1, 1, 0, 0, 0, DateTimeKind.Utc );
+            var json = JsonConvert.Serialize( new Test { Value = time } );
+
+            Assert.Equal( "{\"Value\":946684800}", json );
+
+            var deserializedTime = JsonConvert.Deserialize<Test>( json ).Value;
+
+            Assert.Equal( time, deserializedTime );
+        }
+
+
+        private class Test
+        {
+            [JsonConverter( typeof( UnixDateTimeConverter ) )]
+            public DateTime Value { get; set; }
+        }
+    }
+}
