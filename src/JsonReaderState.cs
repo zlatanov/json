eebc2f7e66e1b72e07fe5667c81d469b5a0f39f5
Ivan Zlatanov <ivan@zlatanov.net>
2020-02-10 19:43:06 +0200

    Introducing JsonReaderStack to handle depth handling without allocating.

--- src/JsonReaderState.cs
+++ src/JsonReaderState.cs
@@ -5,20 +5,34 @@ namespace Maverick.Json
     /// <summary>
     /// Snapshot of the current state of a <seealso cref="JsonReader"/>.
     /// </summary>
-    public sealed class JsonReaderState
+    public readonly struct JsonReaderState
     {
-        internal JsonReaderState()
+        internal JsonReaderState( JsonReader reader,
+                                  ReadOnlyMemory<Byte> memory,
+                                  Int32 offset,
+                                  JsonToken currentToken,
+                                  JsonToken previousToken,
+                                  Boolean expectComma,
+                                  in JsonReaderStack stack )
         {
+            Reader = reader;
+            Position = reader.Position;
+            Memory = memory;
+            Offset = offset;
+            CurrentToken = currentToken;
+            PreviousToken = previousToken;
+            ExpectComma = expectComma;
+            Stack = stack;
         }
 
 
-        public JsonReader Reader { get; internal set; }
-        public SequencePosition Position { get; internal set; }
-        internal ReadOnlyMemory<Byte> Memory { get; set; }
-        internal Int32 Offset { get; set; }
-        internal  JsonToken CurrentToken { get; set; }
-        internal Boolean ExpectComma { get; set; }
-        internal Int32 LineNumber { get; set; }
-        internal JsonToken[] Tokens { get; set; }
+        public JsonReader Reader { get; }
+        public SequencePosition Position { get; }
+        internal ReadOnlyMemory<Byte> Memory { get; }
+        internal Int32 Offset { get; }
+        internal JsonToken CurrentToken { get; }
+        internal JsonToken PreviousToken { get; }
+        internal Boolean ExpectComma { get; }
+        internal JsonReaderStack Stack { get; }
     }
 }
